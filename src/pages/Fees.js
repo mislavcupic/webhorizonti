5df7a78512bffe893ec3eba94bfa892cc4835be6
mislavcupic <mislav.cupic@gmail.com>
2023-07-19 08:27:59 +0200

    initial

--- src/pages/Fees.js
+++ src/pages/Fees.js
@@ -3,7 +3,7 @@ import Navigation from './Navigation'
 import DataProtection from './DataProtection'
 export default function Fees() {
   return (
-    <><Navigation/>
+    <>
     <div>Kotizacije</div>
      <Routes>
      <Route path="dataprotection" element={<DataProtection/>}></Route>
