ac6078e667fc3d973831ca54ceb0ccd1e0eaa117
mislavcupic <mislav.cupic@gmail.com>
2023-09-24 19:50:36 +0200

    navbar bijela pozadina

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Logo from './Logo';
 import '../App.css';
@@ -6,86 +6,90 @@ import HamburgerIcon from '../assets/media/hamburger-icon.png';
 
 function Navigation() {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const showPrijavaLink = windowWidth > 900 || menuOpen;
+  const showPrijavaLink = true; // Change this based on your condition
 
   return (
-    <Navbar expand="lg" style={{ backgroundColor: 'hsla(232, 87%, 98%, 0.8)', height: '120px' }} variant="dark" className={`modern-navbar sticky-top ${menuOpen ? 'opened' : ''}`}>
+    <Navbar
+      expand="lg"
+      style={{
+        backgroundColor: menuOpen ? 'hsla(232, 87%, 98%, 0.8)' : 'hsla(232, 87%, 98%, 0.8)',
+        height: menuOpen ? 'auto' : '120px',
+      }}
+      variant="dark"
+      className={`modern-navbar sticky-top ${menuOpen ? 'expanded' : ''}`}
+    >
       <Navbar.Brand href="/">
         <Logo alt="" width="30" height="30" className="d-inline-block align-top" type="file" />
         Horizonti
       </Navbar.Brand>
       <Navbar.Toggle onClick={toggleMenu} aria-controls="responsive-navbar-nav" className="menu">
-        <img src={HamburgerIcon} style={{ width: '42px', height: '30px', backgroundColor: 'hsla(210, 70%, 50%, 0.8)' }} alt="Hamburger Menu" className={`hamburger-icon ${menuOpen ? 'opened' : ''}`} />
+        <img
+          src={HamburgerIcon}
+          style={{ width: '42px', height: '30px', backgroundColor: 'hsla(210, 70%, 50%, 0.8)' }}
+          alt="Hamburger Menu"
+          className={`hamburger-icon ${menuOpen ? 'opened' : ''}`}
+        />
       </Navbar.Toggle>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className={`mr-auto ${menuOpen ? 'nav-open' : ''}`}>
-          <Nav.Link href="/" className={`nav-link ${menuOpen ? 'nav-link-open' : ''}`}>Naslovna</Nav.Link>
-          <NavDropdown title="O nama" id="collasible-nav-dropdown">
+          <Nav.Link href="/" className={`nav-link ${menuOpen ? 'nav-link-open' : ''}`}>
+            Naslovna
+          </Nav.Link>
+          <NavDropdown title="O nama" id="collapsible-nav-dropdown">
             <NavDropdown.Item href="/about">O nama</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/about/introductionspeech">Pozdravni govor</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/about/organizingcomettee">Programsko povjerenstvo</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/about/theplaceofevent">Mjesto odrzavanja</NavDropdown.Item>
           </NavDropdown>
+          {/* Add more Nav.Link or NavDropdown items as needed */}
           <NavDropdown title="Kotizacije i smještaj" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/registrationfeesaccommodation">Kotizacije i smještaj</NavDropdown.Item>
-            <NavDropdown.Divider />
+           <NavDropdown.Item href="/registrationfeesaccommodation">Kotizacije i smještaj</NavDropdown.Item>
+             <NavDropdown.Divider />
             <NavDropdown.Item href="/registrationfeesaccommodation/dataprotection">Zaštita podataka</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/registrationfeesaccommodation/eventregistration">Prijava na događaj</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/registrationfeesaccommodation/formsofparticipation">
-              Oblici sudjelovanja
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/registrationfeesaccommodation/inserttoken">
-              Token - prijava predavanja
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/registrationfeesaccommodation/lectureselectionpredb">
-              Predbilježbe
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
+            Oblici sudjelovanja
+             </NavDropdown.Item>
+             <NavDropdown.Divider />
+             <NavDropdown.Item href="/registrationfeesaccommodation/inserttoken">
+               Token - prijava predavanja
+             </NavDropdown.Item>
+             <NavDropdown.Divider />
+             <NavDropdown.Item href="/registrationfeesaccommodation/lectureselectionpredb">
+               Predbilježbe
+             </NavDropdown.Item>
+             <NavDropdown.Divider />
             <NavDropdown.Item href="/registrationfeesaccommodation/createpredavanje">
-              Stvori predavanje
+               Stvori predavanje
             </NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/registrationfeesaccommodation/createpredbiljezba">
               Stvori novu predbiljezbu
             </NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/registrationfeesaccommodation/popissazetaka">
-              Popis sažetaka
-            </NavDropdown.Item>
-          </NavDropdown>
+             <NavDropdown.Item href="/registrationfeesaccommodation/popissazetaka">
+               Popis sažetaka
+             </NavDropdown.Item>
+           </NavDropdown>
+
         </Nav>
 
-         {showPrijavaLink && (
-         <Nav className={`ml-auto ${menuOpen ? 'nav-open' : ''}`}>
-         <Nav.Link href="/registrationfeesaccommodation/eventregistration" className={`nav-link ${menuOpen ? 'nav-link-open' : ''}`}>
-           Prijava
-         </Nav.Link>
-       </Nav>
+        {/* Conditionally render "Prijava" link */}
+        {showPrijavaLink && (
+          <Nav className={`ml-auto ${menuOpen ? 'nav-open' : ''}`}>
+            <Nav.Link
+              href="/registrationfeesaccommodation/eventregistration"
+              className={`nav-link ${menuOpen ? 'nav-link-open' : ''}`}
+            >
+              Prijava
+            </Nav.Link>
+          </Nav>
         )}
       </Navbar.Collapse>
     </Navbar>
@@ -93,91 +97,3 @@ function Navigation() {
 }
 
 export default Navigation;
-// import React, { useState } from 'react';
-// import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-// import Logo from './Logo';
-// import '../App.css'; // You can create a CSS file for custom styling
-// import HamburgerIcon from '../assets/media/hamburger-icon.png'; // Import your hamburger icon image
-
-// function Navigation() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-
-//   return (
-//     <Navbar expand="lg" style={{ backgroundColor: 'hsla(232, 87%, 98%, 0.8)', height: '120px' }} variant="dark" className={`modern-navbar sticky-top ${menuOpen ? 'opened' : ''}`}>
-//       <Navbar.Brand href="/">
-//         <Logo alt="" width="30" height="30" className="d-inline-block align-top" type="file" />
-//         Horizonti
-//       </Navbar.Brand>
-//       <Navbar.Toggle onClick={toggleMenu} aria-controls="responsive-navbar-nav" className="menu">
-//         <img src={HamburgerIcon} style={{width:'42px', height:'30px', backgroundColor: 'hsla(210, 70%, 50%, 0.8)'}} alt="Hamburger Menu" className={`hamburger-icon ${menuOpen ? 'opened' : ''}`} />
-//       </Navbar.Toggle>
-//       <Navbar.Collapse id="responsive-navbar-nav">
-//         <Nav className={`mr-auto ${menuOpen ? 'nav-open' : ''}`}>
-//           <Nav.Link href="/" className={`nav-link ${menuOpen ? 'nav-link-open' : ''}`}>Home</Nav.Link>
-//           <NavDropdown title="About" id="collasible-nav-dropdown">
-//             <NavDropdown.Item href="/about">O nama</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/about/introductionspeech">Pozdravni govor</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/about/organizingcomettee">Programsko povjerenstvo</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/about/theplaceofevent">Mjesto odrzavanja</NavDropdown.Item>
-//           </NavDropdown>
-//           <NavDropdown title="Kotizacije i smještaj" id="collasible-nav-dropdown">
-//             <NavDropdown.Item href="/registrationfeesaccommodation">Kotizacije i smještaj</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/dataprotection">Zaštita podataka</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/eventregistration">Prijava na događaj</NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/formsofparticipation">
-//               Oblici sudjelovanja
-//             </NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/inserttoken">
-//               Token - prijava predavanja
-//             </NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/lectureselectionpredb">
-//               Predbilježbe
-//             </NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/createpredavanje">
-//               Stvori predavanje
-//             </NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/createpredbiljezba">
-//               Stvori novu predbiljezbu
-//             </NavDropdown.Item>
-//             <NavDropdown.Divider />
-//             <NavDropdown.Item href="/registrationfeesaccommodation/popissazetaka">
-//               Popis sažetaka
-//             </NavDropdown.Item>
-//           </NavDropdown>
-//         </Nav>
-      
-
-//       </Navbar.Collapse>
-//       <Nav className={`ml-auto ${menuOpen ? 'nav-open' : ''}`}>
-//   {/* Add the "hide-prijava" class to hide the link on smaller screens */}
-//   <Nav.Link href="/registrationfeesaccommodation/eventregistration" className={`nav-link ${menuOpen ? 'nav-link-open hide-prijava' : ''}`}>
-//     Prijava
-//   </Nav.Link>
-// </Nav>
-
-
-
-
-
-
-
-//     </Navbar>
-//   );
-// }
-
-// export default Navigation;
-
